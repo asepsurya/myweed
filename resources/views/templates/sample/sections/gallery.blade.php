88f0e6fa8dc869c0a26925d6f46fe3f5910afc3e
Asep Surya Somantri <asepsurya1998@gmail.com>
2026-01-19 12:31:58 +0700

    Ignore resources/views/templates

--- resources/views/templates/sample/sections/gallery.blade.php
+++
@@ -1,8 +0,0 @@
-<section class="py-16 text-center">
-    <h2 class="text-2xl font-serif mb-6">Galeri Penganten</h2>
-    <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-        @foreach($galleries ?? [] as $image)
-            <img src="{{ $image }}" class="rounded-lg border border-[#D4AF37]">
-        @endforeach
-    </div>
-</section>
